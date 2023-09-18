48ec2e119e99198d94bf436b902561838da7d056
emmanuelist <emmanuel.paul75@yahoo.com>
2023-09-18 21:11:26 +0100

    Implement _atoi function to convert string to int

--- char_funs.c
+++ char_funs.c
@@ -57,3 +57,38 @@ int _strlen(char *s)
 
 }
 
+
+/**
+ * _atoi - Convert a string to an integer
+ * @s: String
+ *
+ * Return: Integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int res = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;/* Toggle sign if '-' is encountered */
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + (s[i] - '0');
+		}
+		else if (res != 0)
+		{
+			break; /**
+				 * Stop if a non-digit character
+				 * is encountered after passing digits
+				 */
+		}
+		i++;
+	}
+
+	return (sign * res);
+}
