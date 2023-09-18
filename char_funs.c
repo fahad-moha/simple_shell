335f0cd10b7740f760444b806e2bc49961a374c2
emmanuelist <emmanuel.paul75@yahoo.com>
2023-09-18 20:42:38 +0100

    Implement _strncpy function to copy a string

---
+++ char_funs.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * _putchar - writes the characteer c to stdout
+ * @c: The character to print
+ *
+ * ReturnL On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/**
+ * _strncpy -  copy a  string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of characters to copy
+ *
+ * Return: Pointer to the destination string
+ */
+
+char *_strncpy(char *dest, const char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		ddest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
